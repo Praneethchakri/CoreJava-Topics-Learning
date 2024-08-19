e6e3c0c0483cfd90609b2ec9fa022460bd5efc48
DELL <praneeth.chakri26@gmail.com>
2024-08-20 02:07:40 +0530

    Added Comments

--- src/coreJava/lambda/topics/LambdaExpressionPractice.java
+++ src/coreJava/lambda/topics/LambdaExpressionPractice.java
@@ -1,6 +1,7 @@
 package coreJava.lambda.topics;
 
 public class LambdaExpressionPractice {
+	//Lambda Expression Practice Basic
 	public static void main(String[] args) {
 
 		FunctionInterface1 obj1 = () -> System.out.println("checking functional Interface Method ");
@@ -62,9 +63,6 @@ public class LambdaExpressionPractice {
 	
 	public long factorialMethod(int number ) {
 		long factorial = 1;
-		
-		
-		
 		for(int i=1;i<=number;i++) {
 			factorial = factorial*i;
 		}
