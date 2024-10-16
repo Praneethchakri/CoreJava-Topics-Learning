73fb6e07ddb7875c6fdc7391433ba1dbba4d9074
unknown <praneeth.chakri26@gmail.com>
2024-10-16 22:22:00 +0530

    Reflection-Field Class-DynamicAccesswithRestricted Modifiers

---
+++ src/reflection/programs/data/model/Company.java
@@ -0,0 +1,14 @@
+package reflection.programs.data.model;
+
+public class Company {
+	private final String companyName;
+
+	private final short streetNumber;
+
+	public Company(String companyName, short streetNumber) {
+		super();
+		this.companyName = companyName;
+		this.streetNumber = streetNumber;
+	}
+
+}
