6edccad639b77ff4c891588976264d2f606c30eb
unknown <praneeth.chakri26@gmail.com>
2024-10-21 00:30:30 +0530

    Count of Chars in String

--- src/coding/practicals/CountofChars.java
+++ src/coding/practicals/CountofChars.java
@@ -7,8 +7,6 @@ import java.util.stream.Collectors;
 public class CountofChars {
 	
 	public static void main(String[] args) {
-		
-		
 		String str = "Praneeth";
 		Arrays.stream(str.split("")).
 		collect(Collectors.groupingBy(Function.identity(),Collectors.counting())).forEach((x,y)-> System.out.println(x +"-->" + y ));
